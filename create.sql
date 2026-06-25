47111c9be52620a01a1ef02864ae2a750b33dd2b
Bibian <bibian.glez@gmail.com>
2026-06-25 15:32:57 +0200

    create v4

--- create.sql
+++ create.sql
@@ -1,10 +1,16 @@
 CREATE DATABASE IF NOT EXISTS lab_mysql;
 USE lab_mysql;
 
+DROP TABLE IF EXISTS `invoices`;
+DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `cars`;
+DROP TABLE IF EXISTS `salespersons`;
+
+
 
 CREATE TABLE IF NOT EXISTS `salespersons` (
 	`salesperson_id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-	`staff_id` VARCHAR(50) NOT NULL UNIQUE, -- Se cambia a VARCHAR para mantener los ceros a la izquierda (ej. '00001')
+	`staff_id` VARCHAR(50) NOT NULL UNIQUE,
 	`name` VARCHAR(255) NOT NULL,
 	`store` VARCHAR(255) NOT NULL,
 	PRIMARY KEY(`salesperson_id`)
@@ -13,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `salespersons` (
 CREATE TABLE IF NOT EXISTS `cars` (
 	`car_id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 	`vin` VARCHAR(255) NOT NULL UNIQUE,
-	`manufacturer` VARCHAR(255) NOT NULL, 
+	`manufacturer` VARCHAR(255) NOT NULL,
 	`model` VARCHAR(255) NOT NULL,
 	`year` YEAR NOT NULL,
 	`color` VARCHAR(255) NOT NULL,
@@ -24,7 +30,12 @@ CREATE TABLE IF NOT EXISTS `customers` (
 	`customer_id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(255) NOT NULL,
 	`email` VARCHAR(255) NOT NULL,
-	`phone` VARCHAR(50) NOT NULL, 
+	`phone` VARCHAR(50) NOT NULL,
+	`address` VARCHAR(255) NULL,   
+	`city` VARCHAR(255) NULL,
+	`state` VARCHAR(255) NULL,
+	`country` VARCHAR(255) NULL,
+	`zip_code` VARCHAR(50) NULL,
 	PRIMARY KEY(`customer_id`)
 );
 
@@ -34,9 +45,9 @@ CREATE TABLE IF NOT EXISTS `invoices` (
 	`invoice_id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 	`date` DATE NOT NULL,
 	`invoice_n` INTEGER NOT NULL UNIQUE,
-	`customer_id` INTEGER UNSIGNED NOT NULL, 
-	`car_id` INTEGER UNSIGNED NOT NULL,       
-	`salesperson_id` INTEGER UNSIGNED NOT NULL, 
+	`customer_id` INTEGER UNSIGNED NOT NULL,
+	`car_id` INTEGER UNSIGNED NOT NULL,
+	`salesperson_id` INTEGER UNSIGNED NOT NULL,
 	PRIMARY KEY(`invoice_id`)
 );
 
